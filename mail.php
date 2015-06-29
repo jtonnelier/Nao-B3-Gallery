791d01febd53c7577579734fbbbedc4e8f8c5663
mblanchard <git@maloblanchard.com>
2015-06-29 14:32:29 +0200

    refactoring 2

--- mail.php
+++ mail.php
@@ -1,15 +1,21 @@
 ﻿<?php
 
-$executed = false;
+$form = '
+	<form action="mail.php" method="post">
+		<p><label for="name">Votre nom : </label><input type="text" id="name" name="name" /></p>
+		<p><label for="mail">Votre mail : </label><input type="text" id="mail" name="mail" /></p>
+		<p><input type="submit" name="form1" value="Envoyer" /></p>
+	</form>';
+
 if (isset($_POST['form1'])) {
 
 	if (isset($_POST['mail']) && isset($_POST['name'])) {
 
-		$mail   = trim($_POST['mail']);
-		$name   = trim($_POST['name']);
-		$idList = array(1, 2, 3);
-		$bumperCorrepondance = array("bouton central", "pied gauche", "pied droit");
-		$randId = rand(0,  count($idList) - 1);
+		$mail      = trim($_POST['mail']);
+		$name      = trim($_POST['name']);
+		$listOfIds = array(1, 2, 3);
+		$bumper    = array("bouton central", "pied gauche", "pied droit");
+		$randomId  = rand(0,  count($listOfIds) - 1);
 
 		try {
 
@@ -20,10 +26,18 @@ if (isset($_POST['form1'])) {
 			$u = $db->prepare('UPDATE emails SET Email_adress = :mail, Name = :name WHERE ID=:id');
 			$u->bindValue(':mail', $mail, PDO::PARAM_STR);
 			$u->bindValue(':name', $name, PDO::PARAM_STR);
-			$u->bindValue(':id', $idList[$randId], PDO::PARAM_INT);
-			$u->execute();
+			$u->bindValue(':id', $listOfIds[$randomId], PDO::PARAM_INT);
 
-			$executed = true;
+			$form = $u->execute() ? "
+			<br />
+			Bonjour ".htmlspecialchars($name).",<br />
+			Votre email porte le numéro <big>".$listOfIds[$randomId].".<big><br />
+			<br />
+			Touchez le ".$bumper[$randomId]." d'HAL pour prendre votre photo.<br />
+			<br />
+			<img src='./images".$listOfIds[$randomId].".png' alt='Bumper' />" :
+			"Une erreur est survenue, merci de ré-essayer dans un instant !";
+
 		} catch(PDOException $error) {
 			echo "PDO ERROR CODE: ".$error->getCode();
 		}
@@ -34,29 +48,32 @@ if (isset($_POST['form1'])) {
 ?>
 
 <html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Inscription NAOmaton</title>
+		<style>
+			.nao {
+				width:400px;
+				height:250px;
+				border: 2px solid #000000;
+				position: absolute;
+				margin: auto;
+				top: 0;
+				right: 0;
+				bottom: 0;
+				left: 0;
+			}
+			.nao div {text-align: center;}
+		</style>
+	</head>
 	<body>
 		<a href="javascript:history.go(-1)">Retour à la galerie</a>
-		<div id="centralBlock" style="width:400px; height:250px; border: 2px solid #000000; position: absolute; margin: auto; top: 0; right: 0; bottom: 0; left: 0;">
-			<div id="title" style="text-align: center;">
+		<div class="nao">
+			<div>
 				Inscription Naomaton
 			</div>
-			<div id="naoForm" style="text-align: center;">
-			<?php 
-				if(!$executed){
-					echo"<form action=\"mail.php\" method=\"post\">
-							<p>Votre Nom : <input type=\"text\" name=\"name\" /></p>
-							<p>Votre Email : <input type=\"text\" name=\"email\" /></p>
-							<p><input type=\"submit\" value=\"Envoyer\"></p>
-						</form>";
-				}
-				else{
-					echo "<br /> Bonjour ".htmlspecialchars($name).",<br />
-						Votre email porte le numéro : ".$idList[$randId].".<br /><br />
-						Toucher le ".$bumperCorrepondance[$randId]." d'HAL pour prendre votre photo.";
-					echo "<img src=\"images/".$idList[$randId].".png\" alt='Bumper' />";
-				}
-				
-			?>
+			<div>
+			<?php echo $form; ?>
 			</div>
 		<div>
 	</body>
